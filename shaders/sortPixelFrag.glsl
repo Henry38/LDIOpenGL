328e9ff5e8e0be8c6e911ea93f065091b65cab3b
Henry38 <lefevrehenry@hotmail.fr>
2016-10-30 02:31:40 +0200

    Single process sort algorithm

--- shaders/sortPixelFrag.glsl
+++ shaders/sortPixelFrag.glsl
@@ -20,19 +20,47 @@ layout (std430, binding = 8) buffer ssbo_indexFrag
 
 uniform uint nb_pixel;
 
+bool compareTo(uint i, uint j)
+{
+    pixel_frag frag1 = pixelFrag[i];
+    pixel_frag frag2 = pixelFrag[j];
+
+    if (frag1.m_j < frag2.m_j)
+        return true;
+    if (frag1.m_j > frag2.m_j)
+        return false;
+
+    if (frag1.m_i < frag2.m_i)
+        return true;
+    if (frag1.m_i > frag2.m_i)
+        return false;
+
+    return (frag1.m_z < frag2.m_z);
+}
+
+void swap(uint i, uint j)
+{
+    pixel_frag tmp = pixelFrag[i];
+    pixelFrag[i] = pixelFrag[j];
+    pixelFrag[j] = tmp;
+}
+
 void main(void)
 {
     uint wid = gl_WorkGroupID.x;
 
-    uint prev = (wid > 0 ? indexFrag[wid - 1] : 0);
-    uint next = indexFrag[wid];
-//    uint prev = 0;
-//    uint next = indexFrag[wid];
-//    if (wid > 0) {
-//        prev = indexFrag[wid - 1];
-//    }
+    uint begin = (wid > 0 ? indexFrag[wid - 1] : 0);
+    uint end = indexFrag[wid];
 
-    for (uint i = prev; i < next; ++i) {
-        // sort pixelFrag
+    for (uint i = begin+1; i < end; ++i) {
+        uint k = i;
+        bool stop = false;
+        while (!stop && k > begin) {
+            stop = compareTo(k-1, k);
+            if (!stop) {
+                swap(k, k-1);
+                k = k - 1;
+            }
+        }
     }
 }
